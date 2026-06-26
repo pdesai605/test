99f3e19e8ec8db37bdd45b5e50427cca0b028b46
ParthAIMachine <pdesai605@gmail.com>
2026-06-26 22:00:25 +0530

    code commit

--- app/components/SmoothScroll.tsx
+++ app/components/SmoothScroll.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { createContext, useContext, useEffect, useRef } from "react";
-import Lenis from "lenis";
+import Lenis from "@studio-freight/lenis";
 import { ScrollTrigger, registerGSAP } from "../lib/gsap-register";
 
 const LenisContext = createContext<Lenis | null>(null);
